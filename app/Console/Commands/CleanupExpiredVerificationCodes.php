9119492f61f4d3d9622c5d11df5d617c55899657
Suhas Dissanayake <suhasdissa@gmail.com>
2025-10-09 12:28:30 +0530

    feat: add command to clean old verification codes

---
+++ app/Console/Commands/CleanupExpiredVerificationCodes.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\VerificationCode;
+use Illuminate\Console\Command;
+
+class CleanupExpiredVerificationCodes extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'verification-codes:cleanup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Delete expired and used verification codes';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+        $deleted = VerificationCode::where(function ($query) {
+            $query->where('expires_at', '<', now())
+                ->orWhere('is_used', true);
+        })->delete();
+
+        $this->info("Deleted {$deleted} expired/used verification codes.");
+
+        return Command::SUCCESS;
+    }
+}
